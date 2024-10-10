447166d47b22358625651f1d2bf3c5d5f69d1513
EthanMM0 <ethan.m.micciola@gmail.com>
2024-10-10 16:08:46 -0400

    Add files via upload

---
+++ precache-manifest.084a8387e8878a13b66d4dfb3c134d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81664788df08878ccd07783790d8690b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311b70d798698fe79230",
+    "url": "/static/css/main.ca8ec99b.chunk.css"
+  },
+  {
+    "revision": "92e23421ff612ea251ce",
+    "url": "/static/js/2.8851f76e.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "311b70d798698fe79230",
+    "url": "/static/js/main.69c13f92.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
